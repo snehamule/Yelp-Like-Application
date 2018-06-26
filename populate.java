b711e36af89a4ecb7aff523b5ba5c293378ac3df
snehamule <ssneham29@gmail.com>
2018-06-26 15:17:57 -0700

    Update populate.java

--- populate.java
+++ populate.java
@@ -3,45 +3,22 @@
 import java.io.IOException;
 import java.sql.SQLException;
 
-import org.json.simple.parser.ParseException;
-
-
 public class populate {
-	String yelpUSerJson ;
-    String businessJson;
-    String reviewJson;
-    String checkinJson;
-	
-	 populate(String businessJson,String reviewJson, String checkinJson,String yelpUSerJson){
-		 this. businessJson= businessJson;
-		 this.reviewJson=reviewJson;
-		 this.checkinJson=checkinJson;
-		 this.yelpUSerJson=yelpUSerJson;
-     }
-	 
+	public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException{
+    	if (args.length !=4) {
+    		System.out.println("Argument must contain 4 files:  business.json, review.json, checki.json, user.json");
+    	}
+    	if (args.length == 4){
+		Business b= new Business(args[0]);
+		Review r = new Review(args[1]);
+		Checkin c = new Checkin(args[2]);
+		Yelp_User y= new Yelp_User(args[3]);
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException, ParseException{
-   	String yelp_business="yelp_business.json";
-    	String yelp_review="yelp_review.json";
-    	String yelp_checkin="yelp_checkin.json";
-    	String yelp_user="yelp_user.json";
-       
-    	//populate p = new populate (yelp_business, yelp_review,yelp_checkin, yelp_user);
-    	
-    	Business b= new Business();
-    	Review r = new Review();
-    	Checkin c = new Checkin();
-    	YelpUser y= new YelpUser();
-
-    	
-    	
-    	//y.populateData();
-    	//b.getBusinessData();
-    	//r.populateReviewData();
-    	c.populateCheckinData();
-
-        
-       
-    }
+		y.populateData();
+		b.getBusinessData();
+		r.populateReviewData();
+		c.populateCheckinData();
+       } 
+ }
     
 }
